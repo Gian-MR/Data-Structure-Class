62d1729b02fa44959feb13212da99f0c5f9cf926
Gian-MR <gian.miranda1@upr.edu>
2026-03-17 08:35:25 -0400

    Lab 1

---
+++ Labs/Lab_1/ArithmeticWrapper.java
@@ -0,0 +1,53 @@
+package Labs.Lab_1;
+
+public class ArithmeticWrapper {
+
+    private static class Arithmetic {
+        private int a;
+        private int b;
+        
+        private Arithmetic (int a, int b){
+            this.a = a;
+            this.b = b;
+        }
+
+        public int getA(){
+            return a;
+        }
+
+        public int getB() {
+            return b;
+        }
+
+        public void setA(int a) {
+            this.a = a;
+        }
+
+        public void setB(int b) {
+            this.b = b;
+        }
+
+        public int add(){
+            return a + b;
+        }
+
+        public int subtract(){
+            return a - b;
+        }
+
+        public int multiply(){
+            return a * b;
+        }
+
+        public double divide(){
+            return a / b;
+        }
+    }
+
+    public static class Calculator extends Arithmetic {
+
+        public Calculator(int a, int b) {
+            super(a, b);
+        }
+    }
+}
