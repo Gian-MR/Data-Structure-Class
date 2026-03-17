5aceb79879cfce15ca60b24b1f3d14332c80b892
Gian-MR <gian.miranda1@upr.edu>
2026-03-17 08:35:39 -0400

    Lab 1

---
+++ Labs/Lab_1/VectorComponentWrapper.java
@@ -0,0 +1,87 @@
+package Labs.Lab_1;
+
+public class VectorComponentWrapper {
+
+    public static interface BaseVector {
+        public double getXComponent();
+
+        public double getYComponent();
+
+        public double getMagnitude();
+
+        public double getAngle();
+
+        public BaseVector add(BaseVector V);
+
+        public BaseVector subtract(BaseVector V);
+
+        public double dotProduct(BaseVector V);
+
+        public BaseVector crossProduct(BaseVector V);
+    }
+
+    public static class VectorComponent implements BaseVector {
+
+        private double X;
+        private double Y;
+
+
+        public VectorComponent(double X, double Y) {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        @Override
+        public double getXComponent() {
+            return X;
+        }
+
+        @Override
+        public double getYComponent() {
+            return Y;
+        }
+
+        public void setX(double x) {
+            this.X = x;
+        }
+
+        public void setY(double y) {
+            this.Y = y;
+        }
+
+        @Override
+        public BaseVector add(BaseVector V) {
+            double newX = X + V.getXComponent();
+            double newY = Y + V.getYComponent();
+
+            return new VectorComponent(newX, newY);
+        }
+
+        @Override
+        public BaseVector subtract(BaseVector V) {
+            double newX = X - V.getXComponent();
+            double newY = Y - V.getYComponent();
+
+            return new VectorComponent(newX, newY);
+        }
+
+        @Override
+        public double dotProduct(BaseVector V) {
+            return X * V.getXComponent() + Y * V.getYComponent(); 
+        }
+
+        @Override
+        public BaseVector crossProduct(BaseVector V) {
+            throw new UnsupportedOperationException("Cross product not defined for 2D vectors");
+        }
+        @Override
+        public double getMagnitude() {
+            return Math.sqrt(X * X + Y * Y);
+        }
+
+        @Override
+        public double getAngle() {
+            return Math.toDegrees(Math.atan2(Y, X)); 
+        }
+    }
+}
