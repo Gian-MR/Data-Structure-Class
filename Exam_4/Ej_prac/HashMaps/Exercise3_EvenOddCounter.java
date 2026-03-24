9eea5e9cf68e546a1d1b9899236bffae1d142a78
Gian-MR <gian.miranda1@upr.edu>
2026-03-24 07:55:38 -0400

    HashMap practice

---
+++ Exam_4/Ej_prac/HashMaps/Exercise3_EvenOddCounter.java
@@ -0,0 +1,28 @@
+package Exam_4.Ej_prac.HashMaps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Exercise3_EvenOddCounter {
+
+    public static Map<String, Integer> countEvenOdd(int[] nums) {
+        // Instructions:
+        // 1. Create a HashMap<String, Integer>.
+        // 2. Put the keys "even" and "odd" with starting value 0.
+        // 3. Loop through the array.
+        // 4. If the number is divisible by 2, increase "even".
+        // 5. Otherwise, increase "odd".
+        // 6. Return the map.
+
+        // ADD CODE HERE
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 4, 7, 10, 3, 8, 1 };
+        System.out.println(countEvenOdd(nums));
+        // Expected idea:
+        // {even=3, odd=3}
+    }
+}
