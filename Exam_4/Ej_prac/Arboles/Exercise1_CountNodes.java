51317143726786be97fb0f5b194cfae5f1092f15
Gian-MR <gian.miranda1@upr.edu>
2026-03-25 09:53:50 -0400

    Arboles

---
+++ Exam_4/Ej_prac/Arboles/Exercise1_CountNodes.java
@@ -0,0 +1,43 @@
+package Exam_4.Ej_prac.Arboles;
+
+public class Exercise1_CountNodes {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static int countNodes(TreeNode root) {
+        // Instructions:
+        // 1. If root is null, return 0.
+        // 2. Recursively count nodes in the left subtree.
+        // 3. Recursively count nodes in the right subtree.
+        // 4. Return 1 + leftCount + rightCount.
+
+        // ADD CODE HERE
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // Tree:
+        //       10
+        //      /  \
+        //     5    15
+        //    / \
+        //   2   7
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(15);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(7);
+
+        System.out.println(countNodes(root));
+        // Expected idea:
+        // 5
+    }
+}
