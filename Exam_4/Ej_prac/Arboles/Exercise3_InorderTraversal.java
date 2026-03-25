31847d53077a128d4df26129bed29db7c996873d
Gian-MR <gian.miranda1@upr.edu>
2026-03-25 09:54:09 -0400

    Arboles

---
+++ Exam_4/Ej_prac/Arboles/Exercise3_InorderTraversal.java
@@ -0,0 +1,49 @@
+package Exam_4.Ej_prac.Arboles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise3_InorderTraversal {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        // Instructions:
+        // 1. Create an ArrayList<Integer> for the result.
+        // 2. Use a helper method for recursive inorder traversal.
+        // 3. Inorder order is: Left -> Root -> Right.
+        // 4. Add visited node values to the list.
+        // 5. Return the list.
+
+        // ADD CODE HERE
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // Tree:
+        //       4
+        //      / \
+        //     2   6
+        //    / \ / \
+        //   1  3 5  7
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(inorderTraversal(root));
+        // Expected idea:
+        // [1, 2, 3, 4, 5, 6, 7]
+    }
+}
