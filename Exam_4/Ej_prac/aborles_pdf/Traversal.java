1ec1b556e619ac253cd689f0406f1a5063e801a9
Gian-MR <gian.miranda1@upr.edu>
2026-04-05 18:06:04 -0400

    pdf exercices

---
+++ Exam_4/Ej_prac/aborles_pdf/Traversal.java
@@ -0,0 +1,17 @@
+package Exam_4.Ej_prac.aborles_pdf;
+
+
+    //!TREE TRAVERSAL RULES (Boat Technique)
+
+    //? Preorder: Root -> Left -> Right
+    // Visit the node when you FIRST arrive (left side)
+    // Example: (7, 2, 9, 4)
+
+    //? Inorder: Left -> Root -> Right
+    // Visit the node when you pass UNDER it (bottom side)
+    // Example: (2, 9, 7, 4)
+
+    //? Postorder: Left -> Right -> Root
+    // Visit the node when you leave it (right side)
+    // Example: (9, 2, 4, 7)
+
