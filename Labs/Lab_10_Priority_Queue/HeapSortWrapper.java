0020c0e5dba5407df1ef2b972949d253275effde
Gian-MR <gian.miranda1@upr.edu>
2026-05-05 07:41:12 -0400

    lab ej

---
+++ Labs/Lab_10_Priority_Queue/HeapSortWrapper.java
@@ -0,0 +1,20 @@
+package Labs.Lab_10_Priority_Queue;
+
+@SuppressWarnings({ "unchecked", "rawtypes" })
+public class HeapSortWrapper {
+
+    /**
+     * 
+     * Implement the Heap Sort Algorithm to sort an array of elements given as
+     * parameter.
+     * 
+     * Note: You must do this IN PLACE, DO NOT USE AN EXTENRAL HEAP/PQ
+     *
+     * @param arr Array to sort
+     * @return The Sorted Array
+     */
+    public static int[] heapSort(int[] arr) {
+        /* TODO ADD YOUR CODE HERE */
+        return null; // Dummy Return
+    }
+}
